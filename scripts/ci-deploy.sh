bd301b2cea77bc70d2ca5b4b7d49563e89bf7d96
nitishdsharma <nitish.d.sharma@gmail.com>
2019-08-23 11:08:51 +0530

    first commit

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
-./kubectl --kubeconfig=/dev/null --server=https://$KUBERNETES_SERVER:6443 --certificate-authority=cert.crt --token=$KUBERNETES_TOKEN apply -f nginx.yml
+./kubectl --kubeconfig=/dev/null --server=https://$KUBERNETES_SERVER:6443 --certificate-authority=cert.crt --token=$KUBERNETES_TOKEN apply -f webservice.yml
